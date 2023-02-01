3f666d49d82ae12c283ca897526f780658a80bc8
Jason Hensley <jasonhensley1989@gmail.com>
2023-01-31 19:30:47 -0600

    full node.js connection for future react project

--- app.js
+++ app.js
@@ -7,10 +7,22 @@
 
 const http = require('http')
 const port = 3000
+const fs = require('fs')
+
 
 const server = http.createServer(function(req, res) {
-    res.write("Hello Node")
-    res.end()
+    res.writeHead(200, { "Content-Type": 'text/html' })
+    fs.readFile('index.html', function(error, data) {
+        if(error) {
+            res.writeHead(404)
+            res.write("Error: File Not Found")
+        } else {
+            res.write(data)
+        }
+        res.end()
+    })
+     
+    
 })
 
 server.listen(port, function(error) {
